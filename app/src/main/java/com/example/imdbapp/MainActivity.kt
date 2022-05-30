ae54233aa2645777aad6f3c26fcb32a680c7fc28
Juan David Verano <jdveranov@gmail.com>
2022-05-30 10:39:07 -0500

    Splash screen correction

--- app/src/main/java/com/example/imdbapp/MainActivity.kt
+++ app/src/main/java/com/example/imdbapp/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.imdbapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import android.view.WindowManager
 import android.widget.ImageView
-import androidx.core.os.HandlerCompat.postDelayed
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
@@ -18,17 +18,19 @@ class MainActivity : AppCompatActivity() {
         //setTheme(R.style.Theme_IMDbApp)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
         this.window.setFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
         val logo = findViewById<ImageView>(R.id.logo_imdb)
 
-        Glide.with(this).load(R.drawable.imdb_logo2016).into(logo)
+        Glide.with(this).load(R.drawable.imdb_logo).into(logo)
 
         changeActivity()
     }
 
     private fun changeActivity(){
-        Handler().postDelayed(Runnable {
+        Handler(Looper.myLooper()!!).postDelayed(Runnable {
             val intent = Intent (this, LoginActivity::class.java )
             startActivity(intent)
         }, duration)
