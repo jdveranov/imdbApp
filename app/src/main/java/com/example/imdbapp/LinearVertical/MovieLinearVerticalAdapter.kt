05ff80940c0a55cafadc7a13ea0c754f0a460000
Juan David Verano <jdveranov@gmail.com>
2022-05-30 23:23:44 -0500

    Adapter setup

---
+++ app/src/main/java/com/example/imdbapp/LinearVertical/MovieLinearVerticalAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.imdbapp.LinearVertical
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.imdbapp.R
+import com.example.imdbapp.commons.Movie
+
+abstract class MovieLinearVerticalAdapter (
+    val data: List<Movie>
+    ) : RecyclerView.Adapter<MovieLinearVerticalViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieLinearVerticalViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+         return  MovieLinearVerticalViewHolder(
+             layoutInflater.inflate(
+                 R.layout.item_view_holder_movie_linear_vertical,
+                 parent,
+                 false
+             )
+         )
+    }
+
+    override fun onBindViewHolder(holder: MovieLinearVerticalViewHolder, position: Int) {
+        val item = data[position]
+        holder.bind(item)
+    }
+
+    override fun getItemCount(): Int = data.size
+    }
+
+}
