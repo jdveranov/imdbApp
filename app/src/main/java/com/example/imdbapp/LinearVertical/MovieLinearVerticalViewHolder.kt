1b78c5a4891b344d6e3c82ffaf3b1adb092ae97c
Juan David Verano <jdveranov@gmail.com>
2022-05-30 22:47:51 -0500

    ViewHolder setup

--- app/src/main/java/com/example/imdbapp/LinearVertical/MovieLinearVerticalViewHolder.kt
+++ app/src/main/java/com/example/imdbapp/LinearVertical/MovieLinearVerticalViewHolder.kt
@@ -1,4 +1,25 @@
 package com.example.imdbapp.LinearVertical
 
-class MovieLinearVerticalViewHolder {
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.imdbapp.commons.Movie
+import com.example.imdbapp.databinding.ItemViewHolderMovieLinearVerticalBinding
+
+class MovieLinearVerticalViewHolder(
+    view: View
+) : RecyclerView.ViewHolder(view){
+
+    private val binding = ItemViewHolderMovieLinearVerticalBinding.bind(view)
+
+    fun bind(model: Movie) {
+
+        with(binding) {
+            Glide.with(itemView).load(model.url).into(imageViewLinearVerticalPrincipal)
+            textViewLinearVerticalTitle.text = model.title
+            textViewLinearVerticalYear.text = model.year.toString()
+            textViewLinearVerticalCasting.text = model.casting
+        }
+    }
 }
+
